d6e3d8d1ec46ad9acc4a795206341e9ffe88bdd2
31grudnia <mikolaj.starczewski@gmail.com>
2024-11-22 22:48:41 +0100

    0.0.6 - Created Comments models for its represetation of tables in DB

--- backend/apps/comments/models.py
+++ backend/apps/comments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class OpportunityComment(models.Model):
+    text = models.CharField(max_length=256)
+    opportunity = models.ForeignKey('opportunities.Opportunity', on_delete=models.CASCADE, related_name='comments')
+
+class ProductComment(models.Model):
+    text = models.CharField(max_length=256)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name='comments')
